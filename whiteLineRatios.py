00eb6eeb76c6fcf65e925f095d36e2b38a0b46d6
Samuel Sottrovisch <114974252+6samu42s9@users.noreply.github.com>
2026-09-15 15:28:48 +0200

    Modify file paths and enhance spectrum plotting
    Updated file paths for different operating systems and added energy axis to the spectrum plot.

--- whiteLineRatios.py
+++ whiteLineRatios.py
@@ -3,9 +3,10 @@ import os
 import tifffile as tiff
 import matplotlib.pyplot as plt
 
-
-
-path = "/Users/moritz/Library/Mobile Documents/com~apple~CloudDocs/Studium/Semester 5/Schlussprojekt/20230908_23136_AbsortionSpectrum/5/"
+#Mac
+#path = "/Users/moritz/Library/Mobile Documents/com~apple~CloudDocs/Studium/Semester 5/Schlussprojekt/20230908_23136_AbsortionSpectrum/5/"
+#Windows Samuel
+path="C:\\Users\\Samuel_Uni\\Documents\\Uni\\Semester V\\PSI_projekt\\XAS_Plots\\10"
 data = []
 files = sorted(os.listdir(path))
 for i in files:
@@ -15,6 +16,9 @@ data = np.array(data)
 globSpectrum = np.sum(data, axis=(1, 2))
 L3_image = np.sum(data[39:90], axis = 0) / 51
 L2_image = np.sum(data[135:199], axis = 0) / 64
+#Select area of interest
+# L3_image = L3_image[100:200, 100:200]
+# L2_image = L2_image[100:200, 100:200]
 
 plt.imshow(L3_image, cmap = "gray")
 plt.show()
@@ -22,5 +26,11 @@ plt.imshow(L2_image, cmap = "gray")
 plt.show()
 plt.imshow(L3_image / L2_image, cmap = "gray")
 plt.show()
-plt.plot(globSpectrum)
+#change x-axis of the spectrum to energy values
+start_energy=700
+end_energy=744
+energy=np.linspace(start_energy, end_energy, len(globSpectrum))
+plt.plot(energy, globSpectrum)
+plt.xlabel("Energy (eV)")
+plt.ylabel("Intensity")
 plt.show()
