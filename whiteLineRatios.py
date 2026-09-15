e4afa7b161084a16477f4a83d203d9b853350769
Samuel Sottrovisch <114974252+6samu42s9@users.noreply.github.com>
2026-09-15 23:15:33 +0200

    Update sample index and adjust file paths
    Load reference spectrum and measured spectrum in the same plot

--- whiteLineRatios.py
+++ whiteLineRatios.py
@@ -1,14 +1,13 @@
-import numpy as np
 import os
-import tifffile as tiff
 import matplotlib.pyplot as plt
+import numpy as np
+import tifffile as tiff
 
-index = 5   #1 to 10, both including. this dictates what sample to analyze
+index = 9   #1 to 10, both including. this dictates what sample to analyze
 #Mac Moritz
-path = "/Users/moritz/Library/Mobile Documents/com~apple~CloudDocs/Studium/Semester 5/Schlussprojekt/20230908_23136_AbsortionSpectrum/" + str(index) + "/"
+#path = "/Users/moritz/Library/Mobile Documents/com~apple~CloudDocs/Studium/Semester 5/Schlussprojekt/20230908_23136_AbsortionSpectrum/" + str(index) + "/"
 #Windows Samuel
-#path="C:\\Users\\Samuel_Uni\\Documents\\Uni\\Semester V\\PSI_projekt\\XAS_Plots\\" + str(index) + "\\"
-
+path="C:\\Users\\Samuel_Uni\\Documents\\Uni\\Semester V\\PSI_projekt\\XAS_Plots\\" + str(index) + "\\"
 energies = np.loadtxt(path[:-2] + "AbsortionSpectrum_23136_" + str(index).zfill(2) + ".csv", delimiter=",", skiprows=1, usecols=0)
 
 data = []
@@ -24,14 +23,37 @@ L2_image = np.sum(data[135:199], axis = 0) / 64
 # L3_image = L3_image[100:200, 100:200]
 # L2_image = L2_image[100:200, 100:200]
 
-plt.imshow(L3_image, cmap = "gray")
-plt.show()
-plt.imshow(L2_image, cmap = "gray")
-plt.show()
-plt.imshow(L3_image / L2_image, cmap = "gray")
-plt.show()
-#change x-axis of the spectrum to energy values
-plt.plot(energies, globSpectrum)
+#plt.imshow(L3_image, cmap = "gray")
+#plt.show()
+#plt.imshow(L2_image, cmap = "gray")
+#plt.show()
+
+relative_absorption =np.nan_to_num(np.divide(L3_image, L2_image), nan=0)
+
+#plt.imshow(relative_absorption, cmap="gray")
+#plt.show()
+
+#Plot single spectrum
+# plt.plot(energies, globSpectrum)
+# plt.xlabel("Energy (eV)")
+# plt.ylabel("Intensity")
+# plt.show()
+
+# # Export the spectrum to a text file
+# np.savetxt(
+#     os.path.join(path[:-3], "globSpectrum.txt"),
+#     np.column_stack((energies, globSpectrum)),
+#     header="Energy (eV)\tIntensity",
+#     fmt="%f\t%f",
+# )
+#Plot multiple spectra
+# Load reference spectrum into folder
+ref_spectrum = np.loadtxt(os.path.join(path[:-3], "refSpectrum_1.txt"), delimiter="\t", skiprows=1)
+plt.plot(ref_spectrum[:, 0], ref_spectrum[:, 1], label="Reference Spectrum")
+plt.plot(energies, globSpectrum, label="Measured Spectrum")
 plt.xlabel("Energy (eV)")
 plt.ylabel("Intensity")
+plt.legend()
 plt.show()
+
+
